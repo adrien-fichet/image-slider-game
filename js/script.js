a2b6f9c56228db7ca16a37d6fbd73741d1a87661
sniksnp <sniksnp@gmail.com>
2014-04-19 13:27:39 +0200

    Use any number of slices

--- js/script.js
+++ js/script.js
@@ -5,22 +5,47 @@ function setViewport(img, x, y, width, height) {
     img.parentNode.style.height = height + 'px';
 }
 
-for (var i=0; i < 9; i++) {
+function createClipImage(imgSrc) {
     var img = document.createElement('img');
-    img.setAttribute('src', 'im/logo.png');
+    img.setAttribute('src', imgSrc);
     img.setAttribute('alt', '');
     img.setAttribute('class', 'clip');
+    return img;
+}
 
+function createImgViewport(img) {
     var div = document.createElement('div');
     div.setAttribute('class', 'viewport');
     div.appendChild(img);
-    document.querySelector('#grid').appendChild(div);
-
+    return div;
 }
 
-var clipImages = document.querySelectorAll('.clip');
+function setViewportOnImages(clipImages, n) {
+    var gridWidth = width(document.querySelector('div#grid'));
+    var clipWidth = Math.floor(gridWidth / n);
 
-for (var i=0; i < clipImages.length; i++) {
-    setViewport(clipImages[i], Math.floor(i / 3) * 100, (i % 3) * 100, 100, 100);
+    for (var i=0; i < clipImages.length; i++) {
+        setViewport(clipImages[i],
+                Math.floor(i / n) * clipWidth,
+                (i % n) * clipWidth,
+                clipWidth,
+                clipWidth);
+    }
 }
 
+function width(element) {
+    return parseInt(window.getComputedStyle(element).width);
+}
+
+function createSlices(imgSrc, n) {
+    for (var i=0; i < Math.pow(n, 2); i++) {
+        var img = createClipImage(imgSrc);
+        var imgViewport = createImgViewport(img);
+        document.querySelector('#grid').appendChild(imgViewport);
+    }
+
+    var clipImages = document.querySelectorAll('.clip');
+    setViewportOnImages(clipImages, n);
+}
+
+createSlices('im/logo.svg', 3);
